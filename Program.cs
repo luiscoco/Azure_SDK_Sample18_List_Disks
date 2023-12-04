5b90fc141f84ab0926e956eb98e8f4f9b442d826
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-12-04 21:38:16 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Compute;
+using Azure.ResourceManager.Compute.Models;
+using Azure.ResourceManager.Resources;
+
+//1. Azure Authorization
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+//2. Get the resource group
+string rgName = "myRG";
+ResourceGroupResource resourceGroup = await subscription.GetResourceGroups().GetAsync(rgName);
+
+//3. Get the disk collection from the resource group
+ManagedDiskCollection diskCollection = resourceGroup.GetManagedDisks();
+
+//4. Get a list of the disks
+await foreach (ManagedDiskResource disk in diskCollection.GetAllAsync())
+{
+    Console.WriteLine(disk.Data.Name);
+}
+
